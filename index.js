036aac97c52d2b7d71eb0e0814e1971abe520027
surendra <smeena1@es.iitr.ac.in>
2022-06-24 09:43:58 -0700

    6/24/2022 moving car first evolution

---
+++ index.js
@@ -0,0 +1,10 @@
+// var audio = document.createElement('audio');
+// audio.setAttribute('src', 'sound.mp3'); 
+// audio.loop=true;
+// audio.play();
+// document.getElementById('myAudioTagID').play();
+
+//play audio with out html audio tag
+
+var myAudio = new Audio('sound.mp3');
+myAudio.play();
